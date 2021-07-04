96c7c4d99d81a851a065c8cff5884512cdfe4875
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-07-03 20:56:27 -0500

    change

--- db/index.js
+++ db/index.js
@@ -1,13 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const devConfig = {
+/* const devConfig = {
 	user: process.env.PGUSER,
 	host: process.env.POGHOST,
 	database: process.env.PGDATABASE,
 	password: process.env.PGPASSWORD,
 	port: process.env.PGPORT,
-};
+}; */
+
+const devConfig = `postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}`;
+
 
 const proConfig = {
 	connectionString = process.env.DATABASE_URL //heroku addons
@@ -22,7 +25,11 @@ const proConfig = {
 }); */
 
 //Pool uses default environment variables if empty, or use an object
-const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig);
-module.exports = {
+const pool = new Pool({
+	connectionString:
+	  process.env.NODE_ENV === "production" ? proConfig : devConfig,
+  });
+  
+  module.exports = {
 	query: (text, params) => pool.query(text, params),
 };
