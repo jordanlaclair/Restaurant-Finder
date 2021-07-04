4ba0ac2e182e88e522a0ae096cd163fcdca937c6
Jordan Molina <jordanmichaelmolina3@gmail.com>
2021-07-04 12:41:24 -0500

    changed logo and title

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
 	return (
 		<div className="container  px-3 py-3">
-			<h1 className=" display-1 text-center mb-4">RESTAURANT FINDER</h1>
+			<h1 className=" display-1 text-center mb-4">RESTAURANT REVIEWER</h1>
 		</div>
 	);
 };
